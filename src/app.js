d86e20a29c9225b1b86ba4d14a582ed978ba9899
David <m.david.corrales@gmail.com>
2022-04-23 18:23:26 -0500

    David - removed console log

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ const getMagnificent = () => {
         .catch(function (error) {
             constructLogFrom(error.response)
         })
-    console.log(aggregate)
 }
 
 const printAggregate = () => {
